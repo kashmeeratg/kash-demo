6da5b3decafed2f2bb3367bd001360cdc1f996ff
arunb <arunb@DESKTOP-VUQTP0Q>
2021-12-19 22:19:34 -0500

    new commit

--- newDemo/src/main/java/pages/Google_Search_Page.java
+++ newDemo/src/main/java/pages/Google_Search_Page.java
@@ -24,7 +24,7 @@ public class Google_Search_Page {
  
  //This is a method to press enter key test more
  public void pressEnter() {
-	 search.sendKeys(Keys.ENTER);
+	 search.sendKeys(Keys.F6);
  }
  
 	
